1798149a78fd9e4748f3bd565d3ba402fd3a219e
Baibhav <baibhavkumar150@gmail.com>
2026-07-16 09:20:08 +0530

    feat: initialize express server and configure vercel deployment settings

--- server/server.js
+++ server/server.js
@@ -52,4 +52,6 @@ app.get("/", (req, res) => {
 // });
 
 
-app.listen(5000,()=>{console.log("server started on port 5000") })
+app.listen(5000,()=>{console.log("server started on port 5000") });
+
+module.exports = app;
